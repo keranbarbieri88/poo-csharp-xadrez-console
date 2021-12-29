c6619d3313064da009546a71702637813a4294af
Keran Barbieri <keranbarbieri@hotmail.com>
2021-12-29 08:17:17 -0300

    Criada a classe Posicao

---
+++ tabuleiro/Posicao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Posicao
+    {
+        //variáveis.
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        //construtor que recebe por parâmentro linha e coluna.
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        //método ToString converte um objeto em sua representação de cadeia de caracteres
+        //para que ele seja adequado para exibição.
+        public override string ToString()
+        {
+            return linha
+                + ", "
+                + coluna;
+        }
+    }
+}
