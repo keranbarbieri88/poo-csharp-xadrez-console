95878b78bc23e83409a8476c3358cfa877cca64f
Keran Barbieri <keranbarbieri@hotmail.com>
2021-12-30 13:28:09 -0300

    Melhorando implementacao do metodo tabuleiro

--- Program.cs
+++ Program.cs
@@ -8,12 +8,28 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+            //tendo executar
+            try 
+            {
 
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            Console.WriteLine(pos.toPosicao());
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 2));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(4, 4));
+
+
+                Tela.imprimirTabuleiro(tab);
+            }
+            //se der algum erro, informo por mensagem
+            catch (TabuleiroException e) 
+            {
+                Console.WriteLine(e.Message);
+            }
            
             Console.ReadLine();
         }
--- Tela.cs
+++ Tela.cs
@@ -12,6 +12,8 @@ namespace xadrez_console
             //contador inicia em 0, enquanto contrato menor que linhas do tabuleiro, contrato incrementa.
             for (int i=0; i<tab.linhas; i++)
             {
+                //imprime os números-guias do tabuleiro
+                Console.Write(8 - i + " ");
                 //contador inicia em 0, enquanto contrato menor que colunas do tabuleiro, contrato incrementa.
                 for (int j=0; j<tab.colunas; j++)
                 {
@@ -23,12 +25,35 @@ namespace xadrez_console
                     else
                     {
                         //acesso a peça, chamando o método peça no objeto tab, linha i, coluna j.
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }                    
                 }
                 //pula uma linha
                 Console.WriteLine();
             }
+            //imprime as letras-guias do tabuleiro
+            Console.WriteLine(" a b c d e f g h");
         }
+
+        //método estático que vai trocar a cor Preta das peças por Amarelo, pois o fundo do console é preto
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                //tipo do C# que pega a cor do sistema, salvo na variável aux
+                ConsoleColor aux = Console.ForegroundColor;
+                //converto em Amarelo
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                //imprimo Amarelo
+                Console.ForegroundColor = aux;
+            }
+        }
+
     }
 }
